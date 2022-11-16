8c29b6277ddbe405ef7df5caef16a5fda8ec4f1f
rachanon khongchai <6210110297@psu.ac.th>
2022-11-16 16:14:26 +0700

    change common classy method

--- project_module/message_classifier.py
+++ project_module/message_classifier.py
@@ -18,7 +18,12 @@ class MessageClassifier:
         text_vec = self.w2v_model.sentence_vectorizer(text_input)
         output = self.model.predict(text_vec)
 
-        return self.classes[output[0]]
+        result = self.classes[output[0]]
+        max_confident = max((self.model.predict_proba(text_vec))[0])
+        if(max_confident < 0.5):
+            return ['C', -1]
+
+        return [result, max_confident]
 
     def __init_json_classes(self, json_classes_path):
         with open(json_classes_path, 'r') as openfile:
