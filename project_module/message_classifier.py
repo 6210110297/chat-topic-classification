861c6d3e922b0b91e158d243bb2e9319ba41d0bc
rachanon khongchai <6210110297@psu.ac.th>
2022-11-22 18:05:59 +0700

    analyz model to auto correct common message

--- project_module/message_classifier.py
+++ project_module/message_classifier.py
@@ -1,13 +1,14 @@
-import re
 import json
+import numpy as np
 from pythainlp.word_vector import WordVector
 from joblib import load
 
 class MessageClassifier:
-    def __init__(self, language='th'):
+    def __init__(self, error_sd= 0.07765331184188104, language='th'):
         self.model = None
         self.w2v_model = WordVector()
         self.classes = None
+        self.error_sd = error_sd
         self.language = language
     
     def load_model(self, model_path, json_classes_path):
@@ -19,11 +20,28 @@ class MessageClassifier:
         output = self.model.predict(text_vec)
 
         result = self.classes[output[0]]
+
+        predict_sd = self.predict_sd(text_vec)
+        print(predict_sd)
+
         max_confident = max((self.model.predict_proba(text_vec))[0])
-        if(max_confident < 0.5):
-            return ['C', -1]
+        # if(max_confident < self.common_offset):
+        #     return [f'{result}->C', (-1 * max_confident)]
 
         return [result, max_confident]
+
+    def predict(self, X):
+        output = self.model.predict(X)
+
+        return output
+
+    def predict_proba(self, X):
+        return   self.model.predict_proba(X)
+
+    def predict_sd(self, X):
+        predict_proba = self.model.predict_proba(X)
+
+        return [ np.std(p) for p in predict_proba ]
 
     def __init_json_classes(self, json_classes_path):
         with open(json_classes_path, 'r') as openfile:
